ba01bbd6958d0e15f2f376614d9f1dbea683761d
MICHAEL <MJENNINGS6@gmail.com>
2021-12-22 11:20:25 -0500

    WIP: clamp method added and tested

--- _.js
+++ _.js
@@ -1,4 +1,11 @@
-const _ = {};
+const _ = {
+  // game plan for implementing .clamp() method -- takes 3 values (number, lower boudn and upper bound)
+  clamp(number, lower, upper) {
+    const lowerClampedValue = Math.max(number, lower);
+    const clampedValue = Math.min(lowerClampedValue, upper);
+    return clampedValue;
+  },
+};
 
 // Do not write or modify code below this line.
 module.exports = _;
